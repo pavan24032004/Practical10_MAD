90b265479b7a4f51db3e46c59e90abf8ebc7a9b7
pavan <123@>
2023-11-05 15:44:05 +0530

    Peac10_5b6_75

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Prac10_5b6_75"
+include(":app")
